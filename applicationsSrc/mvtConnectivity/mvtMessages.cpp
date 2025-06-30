30e3f3399aae08a51a840d5c0c47410eb5de57e0
gabdelah <gilles.abd@gmail.com>
2025-06-30 13:57:31 +0200

    New Algorithm to check for structure mvt connectivity

---
+++ applicationsSrc/mvtConnectivity/mvtMessages.cpp
@@ -0,0 +1,22 @@
+#include "mvtMessages.hpp"
+#include "mvtConnectivityCode.hpp"
+
+
+void FPCheckMessage::handle(BaseSimulator::BlockCode* bc) {
+    mvtConnectivity& mabc = *static_cast<mvtConnectivity*>(bc);
+    Catoms3DBlock* module = mabc.module;
+    Catoms3DWorld* world = Catoms3D::getWorld();
+    P2PNetworkInterface* sender = module->getInterface(senderPos);
+    vector<Cell3DPosition> result;
+    for (const auto& pair : mabc.graphEdges) {
+        const Cell3DPosition& key = pair.first;
+
+        auto it = neighGraph.find(key);
+        if (it != neighGraph.end()) {
+            if (!it->second.empty()) {
+                result.push_back(key);
+                mabc.console << "For the catom at: " << senderPos << " The pos: " << key << " is in common\n";
+            }
+        }
+    }
+}
